38e3b96b0fefffa8eb7d50a1d1999066f7292391
Lee Xiong <74166763+xionglee01@users.noreply.github.com>
2023-08-18 12:47:37 -0700

    Completed Javascript exercises 1 and 2
    Finished javascript exercises 1 and 2 passing all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let new_str = "";
+    
+    if(num < 0)
+        return "ERROR";
 
+    for(let i = 0; i < num; i++)
+    {
+        new_str += string;
+    }
+
+    return new_str;
 };
 
 // Do not edit below this line
