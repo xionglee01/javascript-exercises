c907e67196727c29fbc02790b8d3d79ad6f2a3ab
Lee Xiong <74166763+xionglee01@users.noreply.github.com>
2023-08-18 14:26:45 -0700

    Finished all exercises from 03 - 06
    Completed the exercises from 03 - 06 passing all test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    for(let i = 1; i < arguments.length; i++)
+    {
+        let indextoRemove = arr.indexOf(arguments[i]);
+        if(indextoRemove >= 0)
+        {
+            arr.splice(indextoRemove, 1);
+        }
+    }
+    return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(!(year % 4) && year % 100)
+        return true;
+    else if(!(year % 100) && !(year % 400))
+        return true;
+    return false;
 };
 
 // Do not edit below this line
