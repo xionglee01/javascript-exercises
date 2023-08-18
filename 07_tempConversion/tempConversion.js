cd2f1989a247d7afe4ff6c679790878950f75ee6
Lee Xiong <74166763+xionglee01@users.noreply.github.com>
2023-08-18 14:25:07 -0700

    Finished all exercises up to tempConversion
    exercises 3 - 7 are Finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let celsius = Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
+  return celsius
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(celsius) {
 
+  let fahrenheit = Math.round(((celsius * (9/5)) + 32) * 10) / 10;
+  return fahrenheit;
+};
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
